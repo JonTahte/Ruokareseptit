05c130d5853d0318b90efbe711f9005f0c46817c
JonTahte <niklas.tahtela@gmail.com>
2026-01-30 22:22:14 +0200

    add new recipe form

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import sqlite3
-from flask import Flask
-from flask import redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session
 from werkzeug.security import generate_password_hash, check_password_hash
 import config
 import db
@@ -8,9 +7,48 @@ import db
 app = Flask(__name__)
 app.secret_key=config.secret_key
 
+
 @app.route("/")
-def form():
+def index():
     return render_template("index.html")
+
+@app.route("/new_recipe", methods=['GET', 'POST'])
+def new_recipe():
+    saved_title=""
+    saved_cooking_steps=""
+    ingredients=[""]
+
+    if request.method == 'POST':
+        saved_title=request.form["title"]
+        saved_cooking_steps=request.form["cooking_steps"]
+        ingredients = request.form.getlist("ingredients")
+
+        if 'add' in request.form:
+            ingredients.append('')
+        elif 'remove' in request.form:
+            index_to_remove=int(request.form['remove'])
+            if len(ingredients)>1:
+                ingredients.pop(index_to_remove)
+
+    return render_template("new_recipe.html", ingredients=ingredients, saved_title=saved_title, saved_cooking_steps=saved_cooking_steps)
+
+@app.route("/create_recipe", methods=["POST"])
+def create_recipe():
+    title=request.form["title"]
+    cooking_steps=request.form["cooking_steps"]
+    user_id=session["user_id"]
+
+    sql = """INSERT INTO recipes (title, cooking_steps, user_id)
+        VALUES (?, ?, ?)"""
+    db.execute(sql, [title, cooking_steps, user_id])
+
+    recipe_id=db.last_insert_id()
+    ingredients=request.form.getlist("ingredients")
+    for ingredient in ingredients:
+        sql="""INSERT INTO ingredients (ingredient, recipe_id)
+            Values (?, ?)"""
+        db.execute(sql, [ingredient, recipe_id])
+    return redirect("/")
 
 @app.route("/login", methods=["GET","POST"])
 def login():
@@ -20,21 +58,25 @@ def login():
     if request.method=="POST":
         username=request.form["username"]
         password=request.form["password"]
-        sql = "SELECT password_hash FROM users WHERE username = ?"
-        password_hash = db.query(sql, [username])[0][0]
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result=db.query(sql, [username])[0]
+        user_id=result["id"]
+        password_hash = result["password_hash"]
 
         if check_password_hash(password_hash, password):
+            session["user_id"]=user_id
             session["username"] = username
             return redirect("/")
         else:
             return "VIRHE: väärä tunnus tai salasana"
 
+
 @app.route("/register")
 def register():
     return render_template("register.html")
 
-@app.route("/create", methods=["POST"])
-def create():
+@app.route("/create_account", methods=["POST"])
+def create_account():
     username = request.form["username"]
     password1 = request.form["password1"]
     password2 = request.form["password2"]
@@ -49,7 +91,9 @@ def create():
         return "VIRHE: tunnus on jo varattu"
 
     return "Tunnus luotu"
+
 @app.route("/logout")
 def logout():
+    del session["user_id"]
     del session["username"]
     return redirect("/")
--- schema.sql
+++ schema.sql
@@ -3,3 +3,16 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+create table recipes (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    cooking_steps TEXT,
+    user_id INTEGER REFERENCES users
+);
+
+create table ingredients (
+    id INTEGER PRIMARY KEY,
+    ingredient TEXT,
+    recipe_id INTEGER REFERENCES recipes
+);
