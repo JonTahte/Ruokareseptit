0e242aa54238172bca3f90e4a4393e42eaa6eb8c
JonTahte <niklas.tahtela@gmail.com>
2026-02-14 18:46:30 +0200

    Add find item feature

--- app.py
+++ app.py
@@ -13,6 +13,18 @@ def index():
     all_recipes = recipes.get_recipes()
     return render_template("index.html", recipes=all_recipes)
 
+@app.route("/search_recipe")
+def search_recipe():
+    query = request.args.get("query")
+    if query:
+        results = recipes.find_items(query)
+    else:
+        query = ""
+        results = []
+    return render_template("search_recipe.html",
+                           query=query,
+                           results=results)
+
 @app.route("/recipe/<int:recipe_id>")
 def show_recipe(recipe_id):
     recipe = recipes.get_recipe(recipe_id)
--- recipes.py
+++ recipes.py
@@ -6,11 +6,11 @@ def add_recipe(title, ingredients, cooking_steps, user_id):
     db.execute(sql, [title, ingredients, cooking_steps, user_id])
 
 def get_recipes():
-    sql="SELECT id, title FROM  recipes ORDER BY id DESC;"
+    sql = "SELECT id, title FROM  recipes ORDER BY id DESC;"
     return db.query(sql)
 
 def get_recipe(recipe_id):
-    sql="""SELECT recipes.id,
+    sql = """SELECT recipes.id,
                   recipes.title,
                   recipes.ingredients,
                   recipes.cooking_steps,
@@ -22,13 +22,23 @@ def get_recipe(recipe_id):
     return db.query(sql, [recipe_id])[0]
 
 def update_recipe(recipe_id, title, ingredients, cooking_steps):
-    sql="""UPDATE recipes SET title = ?,
+    sql = """UPDATE recipes SET title = ?,
                             ingredients = ?,
                             cooking_steps = ?
                         WHERE id = ?"""
     db.execute(sql, [title, ingredients, cooking_steps, recipe_id])
 
 def remove_recipe(recipe_id):
-    sql="""DELETE FROM recipes
+    sql = """DELETE FROM recipes
         WHERE id = ?"""
     db.execute(sql, [recipe_id])
+
+def find_items(query):
+    sql = """SELECT id, title
+             FROM recipes
+             WHERE title LIKE ?
+             OR ingredients LIKE ?
+             OR cooking_steps LIKE ?
+             ORDER BY id DESC"""
+    like="%" + query + "%"
+    return db.query(sql, [like, like, like])
