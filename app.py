25a25b142b1daa8b54de96d1beb75c43dc8efb9d
JonTahte <niklas.tahtela@gmail.com>
2026-02-15 14:02:23 +0200

    Refactor user creation/login

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import sqlite3
 from flask import Flask
 from flask import abort, redirect, render_template, request, session
-from werkzeug.security import generate_password_hash, check_password_hash
 import config
 import db
 import recipes
@@ -179,18 +178,15 @@ def login():
     elif request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        sql = "SELECT id, password_hash FROM users WHERE username = ?"
-        result = db.query(sql, [username])[0]
-        user_id = result["id"]
-        password_hash = result["password_hash"]
 
-        if check_password_hash(password_hash, password):
+        user_id = users.check_login(username, password)
+
+        if user_id:
             session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
         else:
             return "VIRHE: väärä tunnus tai salasana"
-
 
 @app.route("/register")
 def register():
@@ -203,12 +199,9 @@ def create_account():
     password2 = request.form["password2"]
     if password1 != password2:
         return "VIRHE: salasanat eivät ole samat"
-    password_hash = generate_password_hash(password1)
 
     try:
-        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
-        db.execute(sql, [username, password_hash])
-
+        users.create_user(username, password1)
     except sqlite3.IntegrityError:
         return "VIRHE: tunnus on jo varattu"
 
